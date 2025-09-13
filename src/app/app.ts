fbc5da81ae5553c625f3c27ef5231291103eb8dd
pejic <kristian.pejich@gmail.com>
2025-09-14 01:18:38 +0200

    app-html structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero';
+import { WerWirSindComponent } from './components/wer-wir-sind/wer-wir-sind';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent],
-  template: `
-    <app-hero></app-hero>
-  `,
+  imports: [
+    HeroComponent,
+    WerWirSindComponent
+  ],
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent {
