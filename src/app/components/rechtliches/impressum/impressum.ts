a1542315bbde54919832ed661ecd3e684e2eea89
dodik <dodikleo48@gmail.com>
2025-10-08 09:57:19 +0200

    feat: Adding impressum and datenschutz subpages

---
+++ src/app/components/rechtliches/impressum/impressum.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-impressum',
+  imports: [],
+  standalone:true,
+  templateUrl: './impressum.html',
+  styleUrl: './impressum.css'
+})
+export class Impressum {
+
+}
